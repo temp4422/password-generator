291c7a0851c7b4c655bfdf8ae727395d7e9bd3a2
user <user@MacBook-Pro-user.local>
2025-02-12 18:24:06 +0200

    Add crypto and words.txt

--- src/script.js
+++ src/script.js
@@ -3,7 +3,17 @@ const copy = document.querySelector('#copy')
 const password = document.querySelector('#password')
 
 generate.addEventListener('click', () => {
-  password.value = Math.random().toString(36).slice(-8)
+  // password.value = Math.random().toString(36).slice(-8)
+  const charset =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&*+,-./:;<=>?@^_|~'
+  const passwordLength = 20
+  const randomValues = new Uint32Array(passwordLength)
+  crypto.getRandomValues(randomValues)
+  let securePassword = ''
+  for (let i = 0; i < passwordLength; i++) {
+    securePassword += charset[randomValues[i] % charset.length]
+  }
+  password.value = securePassword
 })
 
 copy.addEventListener('click', () => {
