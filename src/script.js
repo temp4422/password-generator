96cd511861ec71d12be7c65d17b1742bd832c4a0
user <user@MacBook-Pro-user.local>
2025-02-13 12:49:38 +0200

    WIP

--- src/script.js
+++ src/script.js
@@ -4,37 +4,55 @@ const generateButton = document.querySelector('#generate')
 const copyButton = document.querySelector('#copy')
 const passwordInput = document.querySelector('#password')
 
-generateButton.addEventListener('click', () => {
-  // password.value = Math.random().toString(36).slice(-8)
-  // Constants
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  const upperCaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  const numbers = '0123456789'
-  const symbols = '!#$%&*+,-./:;<=>?@^_|~'
-  const delimeters = '*+,-.=_~'
-  const wordsArray = wordsData.words
+function generatePassword() {
+  const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+  const SYMBOLS = '!#$%&*+,-./:;<=>?@^_|~'
+  const DELIMETERS = '*+,-.=_~'
+  const WORDS_ARRAY = wordsData.words
+  const NUMBER_OF_WORDS = 3
 
-  // Set values
-  const newDelimeter = delimeters[Math.floor(Math.random() * delimeters.length)]
-  const numberOfWords = 3
-
-  // Generate password
-  let password = ''
-  for (let i = 0; i < numberOfWords; i++) {
-    password += wordsArray[Math.floor(Math.random() * wordsArray.length)]
-    // TODO add random capitalization
-    // if (Math.random() > 0.5) password = password.charAt(0).toUpperCase() + password.slice(1)
-    password += newDelimeter
+  function getRandom(length) {
+    return Math.floor(Math.random() * length)
   }
 
-  password += String(Math.floor(Math.random() * 10000)).padStart(4, '0')
-  password += symbols[Math.floor(Math.random() * symbols.length)]
+  // Get random words
+  const words = []
+  for (let i = 0; i < NUMBER_OF_WORDS; i++) {
+    words.push(WORDS_ARRAY[getRandom(WORDS_ARRAY.length)])
+  }
 
-  // Insert password into input
-  passwordInput.value = password
+  // Inset random char in random word
+  let index = getRandom(NUMBER_OF_WORDS)
+  let char = ALPHABET[getRandom(ALPHABET.length)]
+  let charIndex = getRandom(words[index].length)
+  words[index] = words[index].slice(0, charIndex) + char + words[index].slice(charIndex)
+
+  // Capitalize a random word
+  index = getRandom(NUMBER_OF_WORDS)
+  words[index] = words[index].toUpperCase()
+
+  // Inset digits at random position
+  index = getRandom(NUMBER_OF_WORDS + 1)
+  let digits = String(getRandom(10000)).padStart(4, '0')
+  words.splice(index, 0, digits)
+
+  // Add delimeter
+  let delimeter = DELIMETERS[getRandom(DELIMETERS.length)]
+  let symbol = SYMBOLS[getRandom(SYMBOLS.length)]
+  const password = words.join(delimeter).concat(symbol)
+
+  return password
+}
+
+generateButton.addEventListener('click', () => {
+  passwordInput.value = generatePassword()
 })
 
 copyButton.addEventListener('click', () => {
   passwordInput.select()
   navigator.clipboard.writeText(passwordInput.value)
 })
+
+window.onload = function () {
+  passwordInput.value = generatePassword()
+}
