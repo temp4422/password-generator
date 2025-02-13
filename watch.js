92277c915e2b93ca1199f673d812648147e44eef
user <user@MacBook-Pro-user.local>
2025-02-13 16:47:38 +0200

    Rename app.js to server.js

--- watch.js
+++ watch.js
@@ -3,11 +3,11 @@ import { exec } from 'child_process'
 
 fs.watch('./src/', { recursive: true }, (event, filename) => {
   console.log(`Change detected in ${filename}. Running app.js...`)
-  exec('node app.js')
+  exec('node server.js')
   exec('npx @tailwindcss/cli -i ./src/style.css -o ./dist/style.css --watch')
 
   // If need proper error, stdout, stderr handling
-  // exec('node app.js', (error, stdout, stderr) => {
+  // exec('node server.js', (error, stdout, stderr) => {
   //   if (error) return console.error(`Error: ${error.message}`)
   //   if (stderr) return console.error(`stderr: ${stderr}`)
   //   console.log(stdout)
